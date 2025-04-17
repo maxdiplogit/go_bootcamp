bbf0df379de5b8a7bfa2af2412791a5232eb2cc3
maxdiplogit <maxdiplogit@gmail.com>
2025-04-17 23:12:26 +0530

    section 2

--- Section2/investment_calculator.go
+++ Section2/investment_calculator.go
@@ -10,8 +10,8 @@ func main() {
 	var investmentAmount, years float64 = 1000, 10
 	expectedReturnRate := 5.5
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 
 	fmt.Print(futureRealValue)
 }
